41aa45f9d1a92ce63294d18f2f4ebded3b71f8a5
hichemsamti <samtihichem@gmail.com>
2021-08-20 22:39:25 +0100

    cloudinary back end

---
+++ routes/upload.js
@@ -0,0 +1,8 @@
+const router = require("express").Router()
+const uploadImage = require ("../middleware/uploadImage")
+const uploadCtrl = require ("../controllers/uploadCtrl")
+
+router.post('/upload_avatar', uploadImage, uploadCtrl.uploadAvatar )
+
+
+module.exports = router
